e679dc1150551e3235c4f8f79fa346c9ba0c706e
andre9050@yahoo.com <andre9050@yahoo.com>
2022-06-08 10:21:05 -0400

    Working form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,70 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import '@progress/kendo-theme-bootstrap/dist/all.css';
 import { process } from '@progress/kendo-data-query';
+import {
+  Form,
+  Field,
+  FormElement,
+  FieldRenderProps,
+  FormRenderProps,
+} from "@progress/kendo-react-form";
+import { Input } from "@progress/kendo-react-inputs";
 import { Grid, GridColumn } from '@progress/kendo-react-grid';
-import { IReport, IStudent } from './interfaces';
+import { IReport } from './interfaces';
 
 // TODO: Change favicon to something other than the default React logo
 function App() {
 
-  const [student, setStudent] = useState<IStudent>();
+  const [reports, setReports] = useState<IReport[]>([]);
 
   useEffect(() => {
-    const storedStudent = localStorage.getItem('student');
-    setStudent(storedStudent ? JSON.parse(storedStudent) : null);
+    const storedReports = localStorage.getItem('reports');
+    setReports(storedReports ? JSON.parse(storedReports) : []);
   }, []);
 
-  // TODO: Add form to add a new report
+  function addReport(dataItem: { [name: string]: any }) {
+    const newReport: IReport = {
+      studentName: dataItem.studentName,
+      course: dataItem.course,
+      grade: dataItem.grade
+    };
+    setReports([...reports, newReport]);
+    localStorage.setItem('reports', JSON.stringify(reports));
+  }
+
   return (
-    <div className="App" >
-      <Grid data={student?.reports} >
+    <div className="app-container" >
+      <Form onSubmit={addReport}
+        render={(formRenderProps: FormRenderProps) => (
+          <FormElement className={"border-bottom border-dark border-3"}>
+            <fieldset className={"k-form-fieldset"}>
+              <legend className={"k-form-legend"}>
+                Add a new student report:
+              </legend>
+              <Field name={"studentName"} component={Input} label={"Student name"} />
+              <Field name={"course"} component={Input} label={"Course name"} />
+              <Field
+                name={"grade"}
+                type={"number"}
+                component={Input}
+                label={"Grade percentage (0-100)"}
+              />
+            </fieldset>
+            <div className="k-form-buttons" style={{ marginTop: "1rem" }}>
+              <button
+                type={"submit"}
+                className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base"
+                disabled={!formRenderProps.allowSubmit}
+              >
+                Add
+              </button>
+            </div>
+          </FormElement>
+        )}
+      />
+      <Grid data={reports} style={{ marginTop: "2rem" }} >
+        <GridColumn field="studentName" title="Student" />
         <GridColumn field="course" title="Course" />
         <GridColumn field="grade" title="Grade" />
       </Grid>
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,24 +1,8 @@
 
 interface IReport {
+    studentName: string;
     course: string;
     grade: number;
 }
 
-interface IStudent {
-    firstName: string;
-    lastName: string;
-    reports: IReport[];
-}
-
-class Student implements IStudent {
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.reports = [];
-    }
-    firstName: string;
-    lastName: string;
-    reports: IReport[];
-}
-
-export type { IStudent, IReport, Student };
+export type { IReport };
